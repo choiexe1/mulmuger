875a64ce3a8276a93fc7975d44dbd4590c6c4ed3
Junsung Choi <choiexe1@gmail.com>
2025-05-24 19:06:08 +0900

    feat: step slider

---
+++ lib/presentation/widgets/step_slider.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class StepSlider extends StatefulWidget {
+  const StepSlider({
+    required this.labelSuffix,
+    required this.max,
+    required this.interval,
+    required this.onChanged,
+    super.key,
+    this.min = 0,
+    this.value = 0,
+    this.padding = 0,
+  });
+
+  final String labelSuffix;
+  final double value;
+  final double max;
+  final double min;
+  final double interval;
+  final double padding;
+  final ValueChanged<double> onChanged;
+
+  @override
+  State<StatefulWidget> createState() => _StepSliderState();
+}
+
+class _StepSliderState extends State<StepSlider> {
+  late double _value;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _value = (widget.value / widget.interval).round() * widget.interval;
+
+    if (_value < widget.min) {
+      _value = widget.min;
+    }
+
+    if (_value > widget.max) {
+      _value = widget.max;
+    }
+  }
+
+  void _setValue(double newValue) {
+    setState(() {
+      _value = (newValue / widget.interval).round() * widget.interval;
+
+      if (_value < widget.min) _value = widget.min;
+      if (_value > widget.max) _value = widget.max;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final divisions = ((widget.max - widget.min) / widget.interval).round();
+
+    return Slider(
+      padding: widget.padding != 0
+          ? EdgeInsets.all(widget.padding)
+          : EdgeInsets.zero,
+      activeColor: Colors.blue,
+      value: _value,
+      min: widget.min,
+      max: widget.max,
+      divisions: divisions,
+      label: '${_value.round()}${widget.labelSuffix}',
+      onChanged: _setValue,
+      onChangeEnd: widget.onChanged,
+    );
+  }
+}
---
+++ lib/widgetbook/widgets/step_slider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mulmuger/presentation/widgets/step_slider.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+@widgetbook.UseCase(name: 'Default', type: StepSlider)
+Widget build(BuildContext context) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      StepSlider(
+        labelSuffix: context.knobs.string(
+          label: '라벨',
+          description: '슬라이더 변경 시 라벨 뒤에 붙을 텍스트',
+          initialValue: '분',
+        ),
+        max: context.knobs.double.input(label: '최대 값', initialValue: 60),
+        min: context.knobs.double.input(label: '최소 값', initialValue: 5),
+        interval: context.knobs.double.input(label: '인터벌', initialValue: 5),
+        padding: context.knobs.double.input(label: '패딩', description: '기본 값 0'),
+        onChanged: (double value) {},
+      ),
+    ],
+  );
+}
