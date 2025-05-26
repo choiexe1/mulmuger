86a14e017a0d67ff4673174b05bd8891f273786b
Junsung Choi <choiexe1@gmail.com>
2025-05-26 19:55:18 +0900

    feat: local notifier init

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:mulmuger/app.dart';
 import 'package:mulmuger/config/app_config.dart';
+import 'package:mulmuger/config/local_notifier.dart';
 
-void main() {
-  final AppConfig appConfig = AppConfig.initialize(
-    appName: '물머거',
-    flavor: Flavor.dev,
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final localNotifier = LocalNotifier();
+  await localNotifier.initNotification();
+
+  final appConfig = AppConfig.initialize(appName: '물머거', flavor: Flavor.dev);
 
   runApp(App(appConfig: appConfig));
 }
