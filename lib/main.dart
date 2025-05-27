14ea99117f7b814d97f01d91514c8dfb92ab4768
Junsung Choi <choiexe1@gmail.com>
2025-05-27 15:06:18 +0900

    feat: 의존성 주입 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:mulmuger/app.dart';
 import 'package:mulmuger/config/app_config.dart';
+import 'package:mulmuger/core/injection_container.dart';
 
 void main() {
-  final AppConfig appConfig = AppConfig.initialize(
-    appName: '물머거',
-    flavor: Flavor.prod,
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  dependencyInjection();
 
+  final appConfig = AppConfig.initialize(appName: '물머거', flavor: Flavor.prod);
   runApp(App(appConfig: appConfig));
 }
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:mulmuger/app.dart';
 import 'package:mulmuger/config/app_config.dart';
-import 'package:mulmuger/config/local_notifier.dart';
+import 'package:mulmuger/core/injection_container.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  final localNotifier = LocalNotifier();
-  await localNotifier.initNotification();
+  dependencyInjection();
 
   final appConfig = AppConfig.initialize(appName: '물머거', flavor: Flavor.dev);
-
   runApp(App(appConfig: appConfig));
 }
