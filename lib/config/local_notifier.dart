78a203804d4361c66948f48dcc737afa9290321d
Junsung Choi <choiexe1@gmail.com>
2025-05-27 14:43:29 +0900

    feat: local notifier 캡슐화

--- lib/config/local_notifier.dart
+++ lib/config/local_notifier.dart
@@ -1,49 +1,62 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
-import 'package:timezone/timezone.dart' show TZDateTime;
+import 'package:timezone/timezone.dart' as tz;
+
+enum NotificationLevel {
+  immediate(0),
+  scheduled(1),
+  periodic(2);
+
+  const NotificationLevel(this.id);
+
+  final int id;
+}
 
 class LocalNotifier {
   static const String notificationIcon = '@mipmap/ic_launcher';
-  final notificationPlugin = FlutterLocalNotificationsPlugin();
+  static const String location = 'Asia/Seoul';
+  final _notificationPlugin = FlutterLocalNotificationsPlugin();
 
   bool _isInitialized = false;
 
   bool get isInitialized => _isInitialized;
 
-  Future<void> initNotification() async {
+  Future<void> initialize() async {
     if (_isInitialized) return;
 
     tz.initializeTimeZones();
-
-    tz.setLocalLocation(tz.getLocation('Asia/Seoul'));
+    tz.setLocalLocation(tz.getLocation(location));
 
     const androidSettings = AndroidInitializationSettings(notificationIcon);
-
     const initializeSetting = InitializationSettings(android: androidSettings);
 
-    await notificationPlugin.initialize(initializeSetting);
+    await _notificationPlugin.initialize(initializeSetting);
     _isInitialized = true;
   }
 
-  NotificationDetails notificationDetails() {
+  NotificationDetails _notificationDetails() {
     return const NotificationDetails(
       android: AndroidNotificationDetails(
-        'daily_channel_id',
-        'Daily Notifications',
-        channelDescription: 'Daily Notification Channel',
+        'mulmuger_channel_id',
+        '물먹어 알림',
+        channelDescription: '물먹어 알림 채널',
         importance: Importance.max,
         priority: Priority.high,
       ),
     );
   }
 
-  Future<void> showNotification({
-    int id = 0,
+  Future<void> immediateNotification({
+    NotificationLevel level = NotificationLevel.immediate,
     String? title,
     String? body,
   }) async {
-    return notificationPlugin.show(id, title, body, notificationDetails());
+    return _notificationPlugin.show(
+      level.id,
+      title,
+      body,
+      _notificationDetails(),
+    );
   }
 
   Future<void> scheduleNotification({
@@ -51,7 +64,7 @@ class LocalNotifier {
     required String body,
     required int hour,
     required int minute,
-    int id = 1,
+    NotificationLevel level = NotificationLevel.scheduled,
   }) async {
     final now = tz.TZDateTime.now(tz.local);
 
@@ -64,19 +77,39 @@ class LocalNotifier {
       minute,
     );
 
-    await notificationPlugin.zonedSchedule(
-      id,
+    await _notificationPlugin.zonedSchedule(
+      level.id,
       title,
       body,
       scheduledDate,
-      notificationDetails(),
+      _notificationDetails(),
       androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle,
-      // 반복 수행
-      // matchDateTimeComponents: DateTimeComponents.time,
     );
   }
 
+  Future<void> durationallyNotification({
+    required String body,
+    required Duration duration,
+    NotificationLevel level = NotificationLevel.periodic,
+    String? title,
+  }) async {
+    await cancelNotification(level);
+
+    return _notificationPlugin.periodicallyShowWithDuration(
+      level.id,
+      '물먹어',
+      body,
+      duration,
+      _notificationDetails(),
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+    );
+  }
+
+  Future<void> cancelNotification(NotificationLevel level) async {
+    await _notificationPlugin.cancel(level.id);
+  }
+
   Future<void> cancelAllNotifications() async {
-    await notificationPlugin.cancelAll();
+    await _notificationPlugin.cancelAll();
   }
 }
