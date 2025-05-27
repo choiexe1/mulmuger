1953aeefc8264f9d96b74817e91293b12c9718a0
Junsung Choi <choiexe1@gmail.com>
2025-05-27 14:43:42 +0900

    style: lint rule

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,17 +1,17 @@
 enum Flavor { dev, prod }
 
 class AppConfig {
+  const AppConfig._({required this.flavor, required this.appName});
+
   final Flavor flavor;
   final String appName;
 
   static AppConfig? _instance;
 
-  const AppConfig._({required this.flavor, required this.appName});
-
   static AppConfig get instance {
     if (_instance == null) {
       throw Exception(
-        "AppConfig이 초기화 되지 않았습니다. 먼저 AppConfig.initialize()를 호출하세요.",
+        'AppConfig이 초기화 되지 않았습니다. 먼저 AppConfig.initialize()를 호출하세요.',
       );
     }
 
@@ -23,7 +23,7 @@ class AppConfig {
     required String appName,
   }) {
     if (_instance != null) {
-      throw Exception("AppConfig이 이미 초기화 되어있습니다.");
+      throw Exception('AppConfig이 이미 초기화 되어있습니다.');
     } else {
       _instance = AppConfig._(flavor: flavor, appName: appName);
       return instance;
