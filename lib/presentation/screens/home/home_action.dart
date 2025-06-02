cdf473a394191149dcfe5b2d166cd2a455d205dd
Junsung Choi <choiexe1@gmail.com>
2025-06-02 22:47:51 +0900

    feat: home action 추가, 삭제 및 현재 알람 확인

---
+++ lib/presentation/screens/home/home_action.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'home_action.freezed.dart';
+
+@freezed
+sealed class HomeAction with _$HomeAction {
+  const factory HomeAction.onChangedDuration(Duration duration) =
+      OnChangedDuration;
+  const factory HomeAction.checkNotifications() = CheckNotifications;
+  const factory HomeAction.deleteNotifications() = DeleteNotifications;
+}
