66512718c2d22083669c28724a0fec576d21ba53
Junsung Choi <choiexe1@gmail.com>
2025-06-04 03:03:55 +0900

    feat: injection_container -> service locator로 변경하고, DI

---
+++ lib/core/service_locator.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
+import 'package:mulmuger/config/local_notification.dart';
+import 'package:mulmuger/core/logger.dart';
+import 'package:mulmuger/core/router/app_router.dart';
+import 'package:mulmuger/data/services/flutter_local_notification_push_service.dart';
+import 'package:mulmuger/domain/services/local_push_service.dart';
+import 'package:mulmuger/domain/use_cases/set_duration_push_use_case.dart';
+import 'package:mulmuger/presentation/screens/home/home_view_model.dart';
+
+final GetIt sl = GetIt.instance;
+
+Future<void> injection() async {
+  // APP CONFIG
+  sl
+    ..registerSingleton<AppRouter>(AppRouter())
+    ..registerSingleton<Logger>(logger)
+    ..registerSingleton<LocalNotification>(LocalNotification())
+    // SERVICES
+    ..registerLazySingleton<LocalPushService>(
+      () => FlutterLocalNotificationPushService(
+        plugin: FlutterLocalNotificationsPlugin(),
+      )..initialize(),
+    )
+    // USE CASES
+    ..registerLazySingleton<SetDurationPushUseCase>(
+      () => SetDurationPushUseCase(sl()),
+    )
+    // VIEW MODELS
+    ..registerFactory<HomeViewModel>(() => HomeViewModel(sl()));
+}
