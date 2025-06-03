4ee3146a9a723fb63d5f70c1421af28619fe0f1a
Junsung Choi <choiexe1@gmail.com>
2025-06-04 03:42:03 +0900

    feat: 유즈케이스 DI 및 주석 잘 보이게 추가

--- lib/core/service_locator.dart
+++ lib/core/service_locator.dart
@@ -6,27 +6,45 @@ import 'package:mulmuger/core/logger.dart';
 import 'package:mulmuger/core/router/app_router.dart';
 import 'package:mulmuger/data/services/flutter_local_notification_push_service.dart';
 import 'package:mulmuger/domain/services/local_push_service.dart';
+import 'package:mulmuger/domain/use_cases/cancel_notifications_use_case.dart';
+import 'package:mulmuger/domain/use_cases/find_pending_notifications_use_case.dart';
 import 'package:mulmuger/domain/use_cases/set_duration_push_use_case.dart';
 import 'package:mulmuger/presentation/screens/home/home_view_model.dart';
 
 final GetIt sl = GetIt.instance;
 
 Future<void> injection() async {
+  // ==========
   // APP CONFIG
+  // ==========
   sl
     ..registerSingleton<AppRouter>(AppRouter())
     ..registerSingleton<Logger>(logger)
     ..registerSingleton<LocalNotification>(LocalNotification())
+    // ========
     // SERVICES
+    // ========
     ..registerLazySingleton<LocalPushService>(
       () => FlutterLocalNotificationPushService(
         plugin: FlutterLocalNotificationsPlugin(),
       )..initialize(),
     )
+    // =========
     // USE CASES
+    // =========
     ..registerLazySingleton<SetDurationPushUseCase>(
       () => SetDurationPushUseCase(sl()),
     )
+    ..registerLazySingleton<CancelNotificationsUseCase>(
+      () => CancelNotificationsUseCase(sl()),
+    )
+    ..registerLazySingleton<FindPendingNotificationsUseCase>(
+      () => FindPendingNotificationsUseCase(sl()),
+    )
+    // ===========
     // VIEW MODELS
-    ..registerFactory<HomeViewModel>(() => HomeViewModel(sl()));
+    // ===========
+    ..registerFactory<HomeViewModel>(
+      () => HomeViewModel(sl(), sl(), sl(), sl()),
+    );
 }
