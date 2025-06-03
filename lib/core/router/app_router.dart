6d8454dbb57730387d8272efd403517daba1876a
Junsung Choi <choiexe1@gmail.com>
2025-06-04 03:04:32 +0900

    feat: app router에 딥링크를 핸들하는 메서드 추가

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,25 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:mulmuger/core/router/app_router.gr.dart';
+import 'package:mulmuger/core/router/routes.dart';
+import 'package:mulmuger/core/service_locator.dart';
+import 'package:mulmuger/presentation/screens/home/home_view_model.dart';
+
+@AutoRouterConfig(replaceInRouteName: 'ScreenRoot,Route')
+class AppRouter extends RootStackRouter {
+  @override
+  RouteType get defaultRouteType => const RouteType.material();
+
+  @override
+  List<AutoRoute> get routes => [
+    AutoRoute(page: HomeRoute.page, path: AppRoutes.home, initial: true),
+  ];
+
+  DeepLink deepLinkHandler(PlatformDeepLink deepLink) {
+    if (deepLink.path.startsWith(AppRoutes.home)) {
+      final viewModel = sl<HomeViewModel>();
+      return DeepLink([HomeRoute(viewModel: viewModel)]);
+    }
+
+    return DeepLink.defaultPath;
+  }
+}
