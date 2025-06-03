3f5662c66ef97499f8fe8d8548efafde0463e5ec
Junsung Choi <choiexe1@gmail.com>
2025-06-04 03:01:51 +0900

    feat: home screen root가 뷰 모델을 주입받도록 다시 변경

---
+++ lib/presentation/screens/home/home_screen_root.dart
@@ -0,0 +1,39 @@
+import 'package:auto_route/annotations.dart';
+import 'package:flutter/material.dart';
+import 'package:mulmuger/presentation/screens/home/home_action.dart';
+import 'package:mulmuger/presentation/screens/home/home_screen.dart';
+import 'package:mulmuger/presentation/screens/home/home_view_model.dart';
+
+@RoutePage()
+class HomeScreenRoot extends StatefulWidget {
+  const HomeScreenRoot({required this.viewModel, super.key});
+
+  final HomeViewModel viewModel;
+
+  @override
+  State<HomeScreenRoot> createState() => _HomeScreenRootState();
+}
+
+class _HomeScreenRootState extends State<HomeScreenRoot> {
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: widget.viewModel,
+      builder: (BuildContext context, Widget? child) {
+        return HomeScreen(
+          state: widget.viewModel.state,
+          onAction: (HomeAction action) async {
+            switch (action) {
+              case OnChangedDuration():
+                await widget.viewModel.onAction(action);
+              case CheckNotifications():
+                await widget.viewModel.onAction(action);
+              case DeleteNotifications():
+                await widget.viewModel.onAction(action);
+            }
+          },
+        );
+      },
+    );
+  }
+}
