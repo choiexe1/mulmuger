ad0a4d8073373e243ca6ee97e425bdc4a35d5e61
Junsung Choi <choiexe1@gmail.com>
2025-06-04 03:42:19 +0900

    feat: notificaiton config

---
+++ lib/config/notification_config.dart
@@ -0,0 +1,6 @@
+class NotificationConfig {
+  static const String channelId = 'mulmuger_channel_id';
+  static const String channelName = '물머거';
+  static const String title = '물머거';
+  static const String defaultMessage = '물 마실 시간이에요! 물머거!';
+}
