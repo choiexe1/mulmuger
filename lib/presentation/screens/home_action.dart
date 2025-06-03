6f41c1ab491bcc7edc4647b27b8dcbbce2806683
Junsung Choi <choiexe1@gmail.com>
2025-06-04 02:56:07 +0900

    rename: home action

--- lib/presentation/screens/home_action.dart
+++
@@ -1,9 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'home_action.freezed.dart';
-
-@freezed
-sealed class HomeAction with _$HomeAction {
-  const factory HomeAction.onChangedDuration(Duration duration) =
-      OnChangedDuration;
-}
