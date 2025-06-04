239071b5160a595bf9612c1b4bc560fb6b251c2f
Junsung Choi <choiexe1@gmail.com>
2025-06-05 01:03:41 +0900

    feat: notification id 제거

--- lib/config/notification_config.dart
+++ lib/config/notification_config.dart
@@ -3,7 +3,4 @@ class NotificationConfig {
   static const String channelName = '물머거';
   static const String title = '물머거';
   static const String defaultMessage = '물 마실 시간이에요! 물머거!';
-  static const int immediateNotificationId = 1;
-  static const int durationNotificationId = 2;
-  static const int scheduledNotificationId = 3;
 }
