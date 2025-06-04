fd463dd038db2495dd0277d7d5d9127e7fe2ee9f
Junsung Choi <choiexe1@gmail.com>
2025-06-05 01:05:27 +0900

    feat: notification_entity에 fromNow, fromJson, toJson 등 편의 메서드 정의

--- lib/domain/entities/notification_entity.dart
+++ lib/domain/entities/notification_entity.dart
@@ -7,6 +7,39 @@ class NotificationEntity {
     required this.duration,
   });
 
+  factory NotificationEntity.fromNow({
+    required int id,
+    required String title,
+    required String body,
+    required Duration duration,
+  }) => NotificationEntity(
+    id: id,
+    title: title,
+    body: body,
+    createdAt: DateTime.now(),
+    duration: duration,
+  );
+
+  factory NotificationEntity.fromJson(Map<String, dynamic> json) {
+    return NotificationEntity(
+      id: json['id'] as int? ?? 0,
+      title: json['title'] as String? ?? '',
+      body: json['body'] as String? ?? '',
+      createdAt: DateTime.parse(json['createdAt'] as String),
+      duration: Duration(milliseconds: json['duration'] as int? ?? 0),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'body': body,
+      'duration': duration.inMilliseconds,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
   final int id;
   final String title;
   final String body;
