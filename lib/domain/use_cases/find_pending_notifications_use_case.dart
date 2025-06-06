5cb1d91d6b445700b53b4dda08b30459bfa8f667
Junsung Choi <choiexe1@gmail.com>
2025-06-06 22:50:02 +0900

    rename: push service -> push repository

--- lib/domain/use_cases/find_pending_notifications_use_case.dart
+++ lib/domain/use_cases/find_pending_notifications_use_case.dart
@@ -1,12 +1,12 @@
 import 'package:mulmuger/domain/entities/notification_entity.dart';
-import 'package:mulmuger/domain/services/local_push_service.dart';
+import 'package:mulmuger/domain/repositories/local_push_repository.dart';
 
 class FindPendingNotificationsUseCase {
-  const FindPendingNotificationsUseCase(this._notificationService);
+  const FindPendingNotificationsUseCase(this._localPushRepository);
 
-  final LocalPushService _notificationService;
+  final LocalPushRepository _localPushRepository;
 
   Future<List<NotificationEntity>> execute() async {
-    return _notificationService.findPendingNotifications();
+    return _localPushRepository.findPendingNotifications();
   }
 }
