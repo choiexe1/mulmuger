271d25c4be40cdd3fc43af5901c215cc3654f152
Junsung Choi <choiexe1@gmail.com>
2025-06-06 22:50:14 +0900

    rename: push service -> push repository

--- lib/domain/repositories/local_push_repository.dart
+++ lib/domain/repositories/local_push_repository.dart
@@ -1,24 +1,31 @@
 import 'package:mulmuger/domain/entities/notification_entity.dart';
+import 'package:mulmuger/domain/notification/notification_action.dart';
 
-abstract interface class LocalPushService {
+abstract interface class LocalPushRepository {
+  Stream<NotificationAction> get notificationActionStream;
+
   Future<void> initialize();
+
   Future<void> immediatePush({
     required String title,
     required String body,
     required int id,
   });
+
+  Future<void> durationPush({
+    required String title,
+    required String body,
+    required Duration duration,
+    required int id,
+  });
+
   Future<void> scheduledPush({
     required String title,
     required String body,
     required int id,
   });
-  Future<void> durationPush({
-    required Duration duration,
-    required String title,
-    required String body,
-    required int id,
-  });
+
+  Future<List<NotificationEntity>> findPendingNotifications();
   Future<void> cancelNotificationById(int id);
   Future<void> cancelNotifications();
-  Future<List<NotificationEntity>> findPendingNotifications();
 }
